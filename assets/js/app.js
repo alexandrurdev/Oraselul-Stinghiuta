816afc12751eb65a39f0ea1234b6daded29aad4b
alexandrurdev <contact@ardev.ro>
2019-11-06 09:52:46 +0200

    init commmit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            loop:true
+        }
+    }
+});
+
+AOS.init();
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
